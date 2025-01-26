26d195d6ca86559611b25f46c8fbd5f779e83376
yasmimSM2007 <maria.moraes6@estudante.ifms.edu.br>
2025-01-25 20:26:36 -0400

    deu certooo

--- cliente/src/pages/Alterar.jsx
+++ cliente/src/pages/Alterar.jsx
@@ -1,10 +1,48 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function Alterar () {
+export default function Alterar() {
 
-    const {id} = useParams();
-    
-    return(
-        <h1>Página Alterar {id}</h1>
+    const { id } = useParams();
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const navigation = useNavigate();
+
+    useEffect(() => {
+    const fetchUsuarios = async () => {
+        const resposta = await fetch('http://localhost:3000/usuarios');
+        const dados = await resposta.json();
+        setUsuarios(dados);
+    };
+    fetchUsuarios();
+}, []);
+
+    const alterar = async(event) => {
+        event.preventDefault();
+        try{
+            await fetch('http://localhost:3000/usuarios/'+ id, 
+                {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'Application/json'},
+                    body: JSON.stringify({
+                        nome: nome,
+                        email: email
+                    })
+                }
+            );
+            navigation('/');
+        }catch{
+            alert('Erro ao alterar');
+        }
+    }
+    return (
+        <form onSubmit={alterar}>
+            <input type="text" value={nome} onChange={(evento)=> setNome(evento.target.value)}/>
+            <input type="text" value={email}onChange={(evento)=> setEmail(evento.target.value)}/>
+            <button>Alterar</button>
+        </form>
     );
+
+    
 }
--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { jsPDF } from "jspdf";
 import "jspdf-autotable";
+import {Link} from "react-router-dom"
+import { Button } from "@mui/material";
+import DeleteForever from '@mui/icons-material/DeleteForever'
 
 export default function Home() {
 
@@ -23,44 +26,58 @@ export default function Home() {
     try{
       await fetch('http://localhost:3000/usuarios/'+id, {
         method: 'DELETE'
-      })
-    }
-    catch{
+      });
+   
+    }catch{
       alert('Ocorreu um erro na aplicação')
     }
-  }
+  };
 
-  const exportarPDF = () => {
+  const exportarPDF = () =>{
     const doc = new jsPDF();
-    const tabela = usuarios.map( usuario => [
+    const tabela = usuarios.map ( usuario =>[
       usuario.id,
       usuario.nome,
       usuario.email
     ]);
-    doc.text("Lista de usuários", 10, 10);
-    doc.autoTable({
-      head: [["id", "Nome", "Email"]],
+    doc.text("Lista de usuarios",10,10);
+    doc.autoTable ({
+      head: [["ID", "Nome", "Email"]],
       body: tabela
-    });
-    doc.save("alunos.pdf")
+  });
+     doc.save("alunos.pdf");
   }
 
   return (
     <div>
-    <button onClick={()=> exportarPDF()}>Gerar PDF</button>
-    <table>
-      <tr>
-        <td>Nome</td>
-        <td>E-mail</td>
-      </tr>
-      {usuarios.map((usuario) =>
-        <tr key={usuario.id}>
-          <td>{usuario.nome}</td>
-          <td>{usuario.email}</td>
-          <td><button onClick={() => removerPessoa(usuario.id)}>X</button></td>
-        </tr>
-      )}
-    </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarios.map((usuario) => (
+            <tr key={usuario.id}>
+              <td>{usuario.nome}</td>
+              <td>{usuario.email}</td>
+              <td>
+                <button onClick={() => removerPessoa(usuario.id)}>
+                  <DeleteForever />
+                </button>
+                <Link to={'/alterar/' + usuario.id}>
+                  <button>Alterar</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Button variant="contained" onClick={() => exportarPDF()}>
+        Gerar PDF
+      </Button>
     </div>
   );
 }
